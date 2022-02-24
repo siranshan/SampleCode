f65bf3d03d5b91316b2af23ea1dc450e774d22bb
siranshan <100010754+siranshan@users.noreply.github.com>
2022-02-24 15:48:39 +0530

    1st

---
+++ SampleCode/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult SubmitLogin(string strUserName,string strPassword)
+        {
+            string strStatus = string.Empty; //01 - Success 00 - failure
+            string strMessage = string.Empty;
+            string strResult = string.Empty;
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return Json(new { Status=strStatus,Message = strMessage,Result=strResult});
+        }
+
+        
+    }
+}
